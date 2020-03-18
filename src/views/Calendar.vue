55182db37286b31efdb8d67b487a8e88af4b8522
Dexter Lohnes <dexterlohnes@gmail.com>
2020-03-17 23:26:10 -0700

    First commit

---
+++ src/views/Calendar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="calendar">
+    <h1>This will be the page for a specific calendar</h1>
+  </div>
+</template>
---
+++ src/views/Category.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="category">
+    <h1>This will be the page for a specific category</h1>
+  </div>
+</template>
